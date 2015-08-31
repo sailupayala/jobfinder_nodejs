9418712fea5a9522ded940d1ebb815feb26e9116
sailakumari <sailakumari.payala@gmail.com>
2015-08-31 04:06:27 +0000

    from heroku

---
+++ test/jobs-data-spec.js
@@ -0,0 +1,9 @@
+var expect=require("chai").expect;
+describe('get jobs',function(){
+    it("it would never empty",function(){
+        jobsList=[];
+        
+        expect(jobsList.length).to.be.at.least(1);
+        
+    });
+});
